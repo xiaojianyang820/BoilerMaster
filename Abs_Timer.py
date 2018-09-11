48f4100e22f6493396c1d3749988637ebcfe0374
茂陵侯 <980890611@qq.com>
2018-09-11 14:28:09 +0800

    类中的方法忘记写self了

--- Abs_Timer.py
+++ Abs_Timer.py
@@ -11,14 +11,14 @@ class Abs_Timer(object):
     __meta__ = ABCMeta
     
     @abstractmethod
-    def now():
+    def now(self):
         """
           该方法可以返回虚拟时钟当前的时刻
         """
         pass
     
     @abstractmethod
-    def sleep(sleepSeconds):
+    def sleep(self,sleepSeconds):
         """
           该方法可以让程序在当前的虚拟时钟下睡眠一定时间
         """
