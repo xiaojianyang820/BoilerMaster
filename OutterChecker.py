885ba71b3a7461aac27a111c43ceb83f9c131373
茂陵侯 <980890611@qq.com>
2018-09-12 14:51:44 +0800

    OutterChecker的抽象实现更改了参数列表

--- OutterChecker.py
+++ OutterChecker.py
@@ -7,7 +7,7 @@ Created on Tue Sep 11 17:13:29 2018
 from abc import abstractmethod
 
 @abstractmethod
-def OutterChecker(predictGroups,newData,downGasAmount,upGasAmount,logDir):
+def OutterChecker(predictGroups,newData,downGasAmount,upGasAmount,logDir,controlLoops):
     
     # 返回值应该依次为下一控制周期设备的启停方案，上一控制周期的总评分，
     #return GasDis,score
